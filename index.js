3c7ac4e704515720488514172d1dff414252f735
Yash Kashyap <59121404+YashKashyap0605@users.noreply.github.com>
2021-05-13 20:56:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+function myFunction() {
+
+  document.getElementById("box1").style.visibility= "visible";
+  document.getElementById("1").style.visibility= "hidden";
+  document.getElementById("2").style.visibility= "hidden";
+  document.getElementById("3").style.visibility= "hidden";
+  document.getElementById("4").style.visibility= "hidden";
+}
+function myFunction2() {
+
+  document.getElementById("box2").style.visibility= "visible";
+  document.getElementById("1").style.visibility= "hidden";
+  document.getElementById("2").style.visibility= "hidden";
+  document.getElementById("3").style.visibility= "hidden";
+  document.getElementById("4").style.visibility= "hidden";
+}
+function myFunction3() {
+
+  document.getElementById("box3").style.visibility= "visible";
+  document.getElementById("1").style.visibility= "hidden";
+  document.getElementById("2").style.visibility= "hidden";
+  document.getElementById("3").style.visibility= "hidden";
+  document.getElementById("4").style.visibility= "hidden";
+}
+function hide()
+{
+	document.getElementById("box1").style.visibility="hidden";
+	document.getElementById("1").style.visibility= "visible";
+  document.getElementById("2").style.visibility= "visible";
+  document.getElementById("3").style.visibility= "visible";
+  document.getElementById("4").style.visibility= "visible";
+}
+function hide2()
+{
+	document.getElementById("box2").style.visibility="hidden";
+	document.getElementById("1").style.visibility= "visible";
+  document.getElementById("2").style.visibility= "visible";
+  document.getElementById("3").style.visibility= "visible";
+  document.getElementById("4").style.visibility= "visible";
+}
+function hide3()
+{
+	document.getElementById("box3").style.visibility="hidden";
+	document.getElementById("1").style.visibility= "visible";
+  document.getElementById("2").style.visibility= "visible";
+  document.getElementById("3").style.visibility= "visible";
+  document.getElementById("4").style.visibility= "visible";
+}
